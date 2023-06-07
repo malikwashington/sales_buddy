c4dc9690eb7a8c0e251b6a2afd82a0fb9b403310
Malik Washington <malikwashington@gmail.com>
2023-06-07 04:03:16 -0400

    got the contact detail page showing correctly, fixing the edit page is next

--- static/js/contacts.js
+++ static/js/contacts.js
@@ -16,55 +16,71 @@ function call(id) {
 }
 
 function openForm(id) {
-  fetch(`/contacts/${id}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const l = document.getElementsByClassName("modal-title")[1].innerHTML = `${data.f_name} ${data.l_name}`
-      console.log(l)
-      document.getElementsByClassName("modal-body")[1].innerHTML = `
+  console.log(typeof (+id))
+  //helper function to populate data, add onclick to delete and add event listener to edit button
+  const populateData = (data) => {
+    const prevNotes = data.notes;
+    document.getElementsByClassName(
+      "modal-title"
+    )[1].innerHTML = `${data.f_name} ${data.l_name}`;
+    document.getElementsByClassName("modal-body")[1].innerHTML = `
       <form id="modal-form">
         <div class="form-group">
-          <div class="row">
+          <div class="row mb-2">
             <div class="col-4 mt-2">
               <label for="phone">Phone Number</label>
             </div>
             <div class="col-8">
-              <input type="text" class="form-control" disabled=true id="phone" name="phone" value="${data.phone ? data.phone : ''}">
+              <input type="text" class="form-control" disabled=true id="phone" name="phone"
+              value="${data.phone ? data.phone : ""}">
             </div>
           </div>
         </div>
         
         <div class="form-group">
-          <div class="row">
+          <div class="row mb-2">
             <div class="col-4 mt-2">
               <label for="email">Email</label>
             </div>
             <div class="col-8">
-              <input type="text" class="form-control" disabled=true id="email" name="email" value="${data.email ? data.email : ''}">
+              <input type="text" class="form-control" disabled=true id="email" name="email"
+              value="${data.email ? data.email : ""}">
             </div>
           </div>
         </div>
         
         <div class="form-group">
-          <div class="row">
+          <div class="row mb-2">
             <div class="col-4 mt-2">
               <label for="company">Company</label>
             </div>
             <div class="col-8">
-              <input type="text" class="form-control" disabled=true id="company" name="company" value="${data.company ? data.company : ''}">
+              <input type="text" class="form-control" disabled=true id="company" name="company"
+              value="${data.company ? data.company : ""}">
             </div>
           </div>
         </div>
         
         <div class="form-group">
-          <div class="row">
+          <div class="row mb-2">
             <div class="col-4 mt-2">
               <label for="linkedin">Linkedin</label>
             </div>
             <div class="col-8">
-              <input type="text" class="form-control" disabled=true id="linkedin" name="linkedin" value="${data.linkedin ? data.linkedin : ''}">
+              <input type="text" class="form-control" disabled=true id="linkedin" name="linkedin"
+              value="${data.linkedin ? data.linkedin : ""}">
+            </div>
+          </div>
+        </div>
+        
+        <div class="form-group">
+          <div class="row mb-2">
+            <div class="col-4 mt-2">
+              <label for="last_contacted">Last Contacted</label>
+            </div>
+            <div class="col-8">
+              <p class="mt-2">${data.last_contacted ? data.last_contacted : ""
+      }</p>
             </div>
           </div>
         </div>
@@ -75,58 +91,82 @@ function openForm(id) {
               <label for="notes">Notes</label>
             </div>
             <div class="col-8">
-              <input type="text" class="form-control" disabled=true id="notes" name="notes" value="${data.notes ? data.notes : ''}">
+              <input type="text" class="form-control" id="notes" name="notes" value="${data.notes ? data.notes : ""
+      }">
             </div>
           </div>
         </div>
 
       </form>
-      `
-      const footer = document.getElementsByClassName("modal-footer")[1]
+      `;
+    
+    const form = document.getElementById("modal-form");
+    const footer = document.getElementsByClassName("modal-footer")[1];
+
+    footer.innerHTML = `
+      <div class="row">
       
-      footer.innerHTML = `
-      <button type="button" class="btn btn-primary" onclick=editContact(${data.contact_id})>Edit</button>
-      <button type="button" class="btn btn-danger" onclick=deleteContact(${data.contact_id})>Delete</button>
-      `
+        <div class="col-4">
+        <button type="button" class="btn btn-danger" onclick=deleteContact(${data.contact_id})>Delete</button>
+        </div>
+       
+        <div class="col-4">
+        <button type="button" class="btn btn-primary" data-bs-toggle='modal' data-bs-target='#login'
+         data-bs-dismiss='modal' id='edit' onclick="editContact(${data.contact_id})" > Edit </button>
+        </div>
+
+        <div class="col-4">
+        <button type="button" class="btn btn-success" disabled=${prevNotes!=form.notes.value} onclick=saveContact(${data.contact_id})>Save</button>
+        </div>
+
+      </div>
+      `;
+    console.log(prevNotes, form.notes.value)
+      document.getElementById("edit").addEventListener("click", () => {
+        editContact(data);
+      });
+    // const save = document.getElementById('save')
+    // if (prevNotes != data.notes && !save.firstChild) {
+    //   save.innnerHTML = `
+    //     <button type="button" class="btn btn-success" onclick=saveContact(${data.contact_id})>Save</button>
+    //   ` }
+    // else if (prevNotes == data.notes && save.firstChild) save.innerHTML = ''
+    
+  }
+
+
+  if(isNaN(+id)) return populateData(id)
+  
+
+  fetch(`/contacts/${id}`)
+    //api call to get contact data
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data)
+        return populateData(data);
     });
 }
 
-function editContact(id) {
-  const contactModal = document.getElementById("login");
-  document.getElementById("contactModal").ariaModal("false");
-  const fields = document.getElementsByClassName("form-control");
-  contactForm = document.getElementById("modal-form");
-  contactForm.action = `/contacts/${id}/edit`;
-  contactForm.method = "POST";
-  // modal.modal("toggle")
+function editContact(data) {
+  console.log('Malik \n',data)
   
-  // const potential = (divName) => {
-  //   newDiv = document.createElement("div").className = "form-group";
-  //   newDiv.innerHTML = `
-  //   <div class="row mb-2">
-  //     <div class="col-4">
-  //       <label for="${divName.toLowerCase()}">${divName}</label>
-  //     </div>
-  //     <div class="col-8">
 
-  //   `
-  // const urgency = document.createElement("div").className = "form-group";
-  // const potential = document.createElement("div").className = "form-group";
-  // const opportunity = document.createElement("div").className = "form-group";
-
-  // contactForm.append(urgency, potential, opportunity);
-  for(const field of fields) field.disabled = false ;
   document.getElementsByClassName("modal-footer")[0].innerHTML = `
-  <button type="button" class="btn btn-info" onclick=openForm(${id})>Cancel</button>
-  <button type="button" class="btn btn-warning" onclick=saveContact(${id})>Save</button>
-  `
+  <button type="button" data-bs-toggle='modal' data-bs-target='#contactModal' 
+      data.bs-dismiss='modal' class="btn btn-info" id='cancel'>Cancel</button>
+  <button type="button" class="btn btn-warning" onclick=saveContact(${data.contact_id})>Save</button>
+  `;
+
+  document.getElementById("cancel").addEventListener("click", () => { openForm(data); });
 }
 
-function saveContact() {
-
+function saveContact(data) {
+  console.log("save contact")
 }
 
 function deleteContact() {
-
+  console.log("delete contact")
 
 }
